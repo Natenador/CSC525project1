b0e21822873d680d4daab556d876893751a4eba0
Natenador <nathankroll44@gmail.com>
2016-12-10 12:31:21 -0600

    Fix coloring to where "grass" is in the Z direction.

--- project3.cpp
+++ project3.cpp
@@ -118,49 +118,51 @@ Box::Box(int x, int y, int z, int side_len, int id)
 void Box::draw()
 {
 	glBegin(GL_POLYGON);
-	glColor3f(0, 1, 0);
 	//top z-x parallel plane
+	glColor3f(0, 1, 0);
 	glVertex3i(x, y + side_len, z + side_len);
+	glColor3f(1, .5, .25);
 	glVertex3i(x, y + side_len, z);
 	glVertex3i(x + side_len, y + side_len, z);
+
+	glColor3f(0, 1, 0);
 	glVertex3i(x + side_len, y + side_len, z + side_len);
 
 
 	glColor3f(1, .5, .25);
 	//far z-y parallel plane
 	glVertex3i(x, y, z);
-	glColor3f(0, 1, 0);
 	glVertex3i(x, y + side_len, z);
+	glColor3f(0, 1, 0);
 	glVertex3i(x, y + side_len, z + side_len);
 	glColor3f(1, .5, .25);
 	glVertex3i(x, y, z + side_len);
 	//far x-y parallel plane
 	glVertex3i(x, y, z);
-	glColor3f(0, 1, 0);
 	glVertex3i(x, y + side_len, z);
 	glVertex3i(x + side_len, y + side_len, z);
-	glColor3f(1, .5, .25);
 	glVertex3i(x + side_len, y, z);
 	//near z-y parallel plane
-	glVertex3i(x + side_len, y, z + side_len);
+
 	glColor3f(0, 1, 0);
-	glVertex3i(x + side_len, y + side_len, z);
+	glVertex3i(x + side_len, y, z + side_len);
 	glColor3f(1, .5, .25);
+	glVertex3i(x + side_len, y + side_len, z);
 	glVertex3i(x + side_len, y, z);
 	glColor3f(0, 1, 0);
 	glVertex3i(x + side_len, y + side_len, z + side_len);
 	//near x-y paralled plane
-	glColor3f(1, .5, .25);
 	glVertex3i(x + side_len, y, z + side_len);
 	glVertex3i(x, y, z + side_len);
-	glColor3f(0, 1, 0);
 	glVertex3i(x, y + side_len, z + side_len);
 	glVertex3i(x + side_len, y + side_len, z + side_len);
 	//far z-x parallel plane
 	glColor3f(1, .5, .25);
 	glVertex3i(x, y, z);
+	glColor3f(0, 1, 0);
 	glVertex3i(x, y, z + side_len);
 	glVertex3i(x + side_len, y, z + side_len);
+	glColor3f(1, .5, .25);
 	glVertex3i(x + side_len, y, z);
 
 
